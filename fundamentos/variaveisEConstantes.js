2f699f3c84bf17c17c56bad5c67a0b7879e64736
Paulo Sergio Jr <paulo.psw@gmail.com>
2019-06-22 17:41:56 -0300

    add variáveis e constantes

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3
+let b = 4
+
+var a = 30  // 'var' é considerado obsoleto, e permite redeclarações
+b = 40      // variáveis declaradas com 'let' não podem ser redeclaradas
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50
+console.log(c)
