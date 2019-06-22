2ec8b75b3a741e296df72ddf85e6908117a17434
Paulo Sergio Jr <paulo.psw@gmail.com>
2019-06-22 18:07:11 -0300

    add Alguns cuidados com números

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0)
+console.log("10" / 2)
+console.log("Show" * 2)
+console.log(0.1 + 0.7) // imprecisão
+// console.log(10.toString()) // não funciona
+console.log((10).toString())  // funciona
