d17d452951877be6d8376f84a6d90e2febb9f8a2
Paulo Sergio Jr <paulo.psw@gmail.com>
2019-06-29 16:58:29 -0300

    Mais cuidados com números

--- fundamentos/numerosAlgunsCuidados.js
+++ fundamentos/numerosAlgunsCuidados.js
@@ -1,6 +1,8 @@
 console.log(7/0)
 console.log("10" / 2)
+console.log('3' + 2) // concatena string '32'
 console.log("Show" * 2)
 console.log(0.1 + 0.7) // imprecisão
 // console.log(10.toString()) // não funciona
 console.log((10).toString())  // funciona
+console.log((10.345).toFixed(2))
