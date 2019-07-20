d4f3f26c53fc287158f1ee5ea95bd1787b294814
Paulo Sergio Jr <paulo.psw@gmail.com>
2019-07-20 19:56:02 -0300

    Cont. Objetos

---
+++ fundamentos/objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object)      // retorna uma funcao
+console.log(typeof new Object)  // retorna um objeto
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Clliente)
+
+class Produto {}
+console.log(typeof Produto)
+console.log(typeof new Produto())
